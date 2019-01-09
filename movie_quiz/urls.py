aa80594e712c9edb03569a908da597454cfdcf37
hyeon seung jae <uiandwe@gmail.com>
2019-01-09 20:59:53 +0900

    template init 추가

--- movie_quiz/urls.py
+++ movie_quiz/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'quiz/', TemplateView.as_view(template_name='index.html'))
 ]
