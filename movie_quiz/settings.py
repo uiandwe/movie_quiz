5b32733c1257dd83b7c574295a3b5281e988172c
hyeon seung jae <uiandwe@gmail.com>
2019-01-10 16:58:44 +0900

    movie app 추가

--- movie_quiz/settings.py
+++ movie_quiz/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'quiz.apps.QuizConfig',
+    'movie.apps.MovieConfig',
     'rest_framework',
     'rest_framework_swagger'
 ]
