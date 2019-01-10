982cbf9ac1cde52930b14b7ad34d14325d75a71b
hyeon seung jae <uiandwe@gmail.com>
2019-01-10 16:52:27 +0900

    core model update

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+
+class TimeStampeModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class FileModel(models.Model):
+    folder = models.CharField(max_length=255)
+    dateFolderPath = models.CharField(max_length=255)
+    fileName = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
