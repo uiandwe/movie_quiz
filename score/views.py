73c63c5ad2cd01f027cfe84286e31ae776025674
hyeon seung jae <uiandwe@gmail.com>
2019-01-14 01:35:51 +0900

    score api 추가

--- score/views.py
+++ score/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Score
+from .serializers import ScoreSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class ScoreList(generics.ListCreateAPIView):
+    queryset = Score.objects.all()
+    serializer_class = ScoreSerializer
+
+
+class ScoreDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Score.objects.all()
+    serializer_class = ScoreSerializer
