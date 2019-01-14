0c3e0362321ca33a2f344e3d45ef6ce3c35c26e0
hyeon seung jae <uiandwe@gmail.com>
2019-01-14 22:33:56 +0900

    서버 시작시 media 폴더 추가

--- movie_quiz/__init__.py
+++ movie_quiz/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-media_path = os.path.dirname(os.path.abspath(__file__)) + "/.."+'media'
+media_path = os.path.dirname(os.path.abspath(__file__)) + "../"+'media'
 
 if not os.path.exists(media_path):
     os.makedirs(media_path)
