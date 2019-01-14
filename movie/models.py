05fc4ad2703f142eed2da1a9b9f016ce949e2046
hyeon seung jae <uiandwe@gmail.com>
2019-01-14 22:47:28 +0900

    movie 모델 수정 + movieFile 모델 추가

--- movie/models.py
+++ movie/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from core.models import TimeStampeModel, FileModel
 
 
-class Movie(TimeStampeModel, FileModel):
+class MovieFile(FileModel):
+    file = models.FileField(blank=True, default='')
+
+
+class Movie(TimeStampeModel):
     title = models.CharField(max_length=100, blank=True, default='')
     desc = models.TextField()
     owner = models.ForeignKey('auth.User', related_name='movie', on_delete=models.CASCADE, null=False)
+    file_id = models.ForeignKey(MovieFile, on_delete=models.CASCADE)
 
