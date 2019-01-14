1c1e5281f38c656d12da161fdd38363d856f2258
hyeon seung jae <uiandwe@gmail.com>
2019-01-14 15:45:05 +0900

    score api test 추가

--- score/test_view.py
+++ score/test_view.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestScoreView(TestCase):
+
+    def test_index(self):
+        response = self.client.get('/score/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_details(self):
+        response = self.client.get('/score/', {'pk': '1'})
+        self.assertEqual(response.status_code, 200)
