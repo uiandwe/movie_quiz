7a3b975523d5a713cbc93e244b5b811a2052bc92
hyeon seung jae <uiandwe@gmail.com>
2019-01-15 21:42:20 +0900

    디비 마이그레이션

--- movie/models.py
+++ movie/models.py
@@ -22,7 +22,7 @@ def path_and_rename(instance, filename):
 
 class MovieFile(models.Model):
     file = models.FileField(blank=True, default='', upload_to=path_and_rename)
-    
+
     class Meta:
         ordering = ['-id']
 
